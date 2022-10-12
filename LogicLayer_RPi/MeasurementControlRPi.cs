af2e5b9e26e3f95b7265043f08d579804121dcba
Camilla Kolby Thomsen <camillakthomsen@hotmail.com>
2022-10-12 08:31:12 +0200

    Oprettet klasser og reference mellem logik og datalag

---
+++ LogicLayer_RPi/MeasurementControlRPi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer_RPi
+{
+    //Modtager data fra datalaget, tjekker alarmstatus og beregner
+    class MeasurementControlRPi
+    {
+    }
+}
